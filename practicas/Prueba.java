c234d6d4437dfdc6b79d6b9057c98d2bed6cfa8b
LibeUwU <159199101+LibeUwU16@users.noreply.github.com>
2024-03-04 16:52:47 -0600

    Add files via upload

---
+++ practicas/Prueba.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Prueba{
+
+           public static void main(String args[]){
+                   Scanner captura = new Scanner(System.in);
+                   System.out.println("Hola mundo");
+                   String nombre = captura.nextLine();
+                   System.out.println(nombre);
+               }
+}
