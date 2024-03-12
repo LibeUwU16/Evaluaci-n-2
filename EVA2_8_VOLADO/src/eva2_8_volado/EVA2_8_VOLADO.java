a23a8564a5b09772f6b06c6d1168c5aaa65c7737
LibeUwU <159199101+LibeUwU16@users.noreply.github.com>
2024-03-12 16:47:16 -0600

    Add files via upload

---
+++ EVA2_8_VOLADO/src/eva2_8_volado/EVA2_8_VOLADO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_8_volado;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_8_VOLADO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Número aleatorio en Java:
+        //Valores entre 0 y 1.
+        double moneda;
+        moneda = Math.random();
+        System.out.println("El valor de moneda = " + moneda);
+        
+        //0.5 es la mitad del rango
+        if(moneda > 0.5)
+            System.out.println("SELLO");
+        else
+            System.out.println("AGUILA");
+    }
+    
+}
