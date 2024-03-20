1e91c9615e859a63cbe329bfb7dc96c02921b6d4
LibeUwU <159199101+LibeUwU16@users.noreply.github.com>
2024-03-20 15:44:41 -0700

    Add files via upload

---
+++ EVA2_15_COMPARAR_TEXTO/src/eva2_15_comparar_texto/EVA2_15_COMPARAR_TEXTO.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_15_comparar_texto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_15_COMPARAR_TEXTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String dia;
+    Scanner captu = new Scanner(System.in);
+        System.out.println("Introduce el día.");
+        dia = captu.nextLine();
+        
+        if(dia.equals("domingo"))
+            System.out.println("1");
+        else if(dia.equals("lunes"))
+            System.out.println("2");
+        else if(dia.equals("martes"))
+            System.out.println("3");
+        else if(dia.equals("miercoles"))
+            System.out.println("4");
+        else if(dia.equals("jueves"))
+            System.out.println("5");
+        else if(dia.equals("viernes"))
+            System.out.println("6");
+        else if(dia.equals("sabado"))
+            System.out.println("7");
+        else
+            System.out.println("Otro día");
+    }
+    
+}
