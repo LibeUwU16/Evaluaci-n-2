13b97ef35dcc956a4f8ee15891ed8f90c907505d
LibeUwU <159199101+LibeUwU16@users.noreply.github.com>
2024-04-09 16:50:57 -0600

    Add files via upload

---
+++ EVA2_18_FOR/src/eva2_18_for/EVA2_18_FOR.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_18_for;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_18_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // INICIO; CONDICIÓN;INCREMENTO/DECREMENTO
+        for(int i = 1; i <= 10; i++){
+            System.out.print(i + "-");
+        }
+        System.out.println("");
+        for(int i = 10; i >= 1; i--){
+            System.out.print( i + "-");
+        } 
+        System.out.println("");
+        for(int i = 0; i <= 100; i+=2){
+            System.out.print(i + "-");
+        }
+    }
+    
+}
